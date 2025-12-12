f23ef74dc7072504be958fd717cbd9de876331ee
realtobi999 <tobiasfilgas@gmail.com>
2025-12-12 11:36:24 +0100

    [feat] size parameter for downloading qr codes

---
+++ src/Api/Properties/Items/GenerateCodes/GenerateCodesPropertyItemsResponse.cs
@@ -0,0 +1,31 @@
+using System.Text.RegularExpressions;
+using Invenire.Common.Errors;
+
+namespace Invenire.Api.Properties.Items.GenerateCodes;
+
+public record GenerateCodesPropertyItemsResponse
+{
+    public static readonly List<ErrorTranslation> ErrorTranslations =
+    [
+        new ErrorTranslation
+        {
+            Pattern = new Regex(@"'size' must not be empty\."),
+            Translate = _ => "Velikost nesmí být prázdná."
+        },
+        new ErrorTranslation
+        {
+            Pattern = new Regex(@"'size' must be greater than or equal to '(\d+)'\."),
+            Translate = m => $"Velikost musí být větší než {m.Groups[1].Value} pixelů."
+        },
+        new ErrorTranslation
+        {
+            Pattern = new Regex(@"'size' must be less than or equal to '(\d+)'\."),
+            Translate = m => $"Velikost musí být menší než {m.Groups[1].Value} pixelů."
+        },
+        new ErrorTranslation
+        {
+            Pattern = new Regex(@".*"),
+            Translate = _ => "Objevila se nečekaná chyba. Zkuste to prosím později."
+        }
+    ];
+}
